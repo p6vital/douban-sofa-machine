bb5a267a60652164d9b5cb3c01b0ae8a2692bb38
p6vital <p6vital@gmail.com>
2017-01-16 21:35:28 -0800

    Update debug-nav.jsx

--- tampermonkey/debug-nav.jsx
+++ tampermonkey/debug-nav.jsx
@@ -1,14 +1,56 @@
-const titleElement = (
-  <div>
-    <span>Debug Mode: {DoubanUtils.debugMode}</span>
-  </div>
-);
-
 class DebugTitle extends React.Component {
   render() {
-    return titleElement;
+    return (
+      <div>
+        <span>Debug Mode: {DoubanUtils.debugMode}</span>
+      </div>
+    );
   }
 }
+
+class InputField extends React.Component {
+  render() {
+    var valid = true;
+
+    if (typeof props.validate === 'function') {
+      props.validate(this.state.input);
+    }
+
+    return (
+      <div className="{!valid ? 'db-invalid' : ''}">
+        <div>
+          {props.label}
+        </div>
+        <div>
+          {props.inputField}
+        </div>
+        <div>
+          {props.invalidMessage}
+        </div>
+      </div>
+    );
+  }
+}
+
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
+
+  render() {
+    return (
+      <InputField 
+        label={this.props.label}
+        validate={this.props.validate}
+        inputFiled={
+          <input type='text' value={this.state.value}/>
+        } />
+      );
+    }
+  }
 
 class HelloMessage extends React.Component {
   render() {
@@ -16,6 +58,13 @@ class HelloMessage extends React.Component {
       <div>
         <DebugTitle/>
         <div>Hello {this.props.name}</div>
+        <TextInput 
+          value=''
+          label='Test Input'
+          validate={(value) => {
+            return value;
+          }}
+        />
       </div>
     );
   }
