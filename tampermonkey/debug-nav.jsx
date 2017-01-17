6fd7c3b4b1b21cdafca6e3ceb680d3e65f0c71cb
p6vital <p6vital@gmail.com>
2017-01-17 01:09:20 -0800

    Update debug-nav.jsx

--- tampermonkey/debug-nav.jsx
+++ tampermonkey/debug-nav.jsx
@@ -13,35 +13,52 @@ class InputField extends React.Component {
     super(props);
     this.state = {
       value: props.value,
+      displayValue: props.value,
+      valid: true,
+      invalidMessage: null,
     };
   }
 
   onChange(event) {
-    this.setState({
-      value: event.target.value,
-      dirty: true,
-    });
+    var newValue = event.target.value;
+    console.log('New Value: ' + newValue);
+    var invalidMessage = this.props.validate(newValue);
+    
+    if (!invalidMessage) {
+      if (typeof this.props.onChange === 'function') {
+        this.props.onChange(newValue);
+      } 
+
+      this.setState({
+        value: newValue,
+        displayValue: newValue,
+        valid: true,
+        invalidMessage: null,
+      });
+    } else {
+      this.setState({
+        displayValue: newValue,
+        valid: false,
+        invalidMessage: invalidMessage,
+      });
+    }
   }
 
   render() {
-    var valid = true;
     var invalidMessageDom;
     
-    if (this.state.dirty && typeof this.props.validate === 'function') {
-      var invalidMessage = this.props.validate(this.state.value);
-      if (invalidMessage) {
-        invalidMessageDom = <div className='db-input-invalid-msg'>{invalidMessage}</div>;
-      }
+    if (!this.state.valid) {
+      invalidMessageDom = <div className='db-input-invalid-msg'>{this.state.invalidMessage}</div>;
     }
 
     return (
-      <div className={'db-input' + (!valid ? ' db-input-invalid' : '')}>
+      <div className={'db-input' + (!this.state.valid ? ' db-input-invalid' : '')}>
         <div className='db-input-label'>
           {this.props.label}
         </div>
         <div className='db-input-field'>
           <div>
-            {this.props.renderInputField(this.state.value, this.onChange.bind(this))}
+            {this.props.renderInputField(this.state.displayValue, this.onChange.bind(this))}
           </div>
           {invalidMessageDom}
         </div>
@@ -58,22 +75,52 @@ class TextInput extends React.Component {
         value={this.props.value}
         invalidMessage={this.props.invalidMessage}
         validate={this.props.validate}
+        onChange={this.props.onChange}
         renderInputField={(value, onChange) => {
           return <input type='text' value={value} onChange={onChange}/>;
-        }} />
-      );
+        }}
+      />
+    );
+  }
+}
+
+class DebugArgs extends React.Component {
+  render() {
+    var doms = [];
+    for (var key in this.props.debugArgs) {
+      doms.push(<div key={key}>{key}: {this.props.debugArgs[key]}</div>);
     }
+    
+    return (
+      <div>
+        <div>Debug Args:</div>
+        {doms}
+      </div>
+    )
+  }
+}
+
+class DebugNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshRate: 10
+    };
   }
 
-class HelloMessage extends React.Component {
+  setDebugArgs(key, value) {
+    var newState = {};
+    newState[key] = value;
+    this.setState(newState);
+  }
+
   render() {
     return (
       <div>
         <DebugTitle/>
-        <div>Hello {this.props.name}</div>
         <TextInput 
           label='Refresh Rate (sec)'
-          value='10'
+          value={this.state.refreshRate}
           validate={(value) => {
             if (isNaN(value) || (parseInt(value, 10) != value)) {
               return 'Not a integer';
@@ -83,10 +130,13 @@ class HelloMessage extends React.Component {
               return 'Not a positive integer'
             }
           }}
+          onChange={(value) => {this.setDebugArgs('refreshRate', parseInt(value, 10))}}
         />
+        
+        <DebugArgs debugArgs={this.state} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<HelloMessage name="John" />, document.getElementById('debug-nav'));
+ReactDOM.render(<DebugNav />, document.getElementById('debug-nav'));
